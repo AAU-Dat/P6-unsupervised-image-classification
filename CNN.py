07cd236d7cde70bc1534db3258f7546441573d3c
Houi <hougaardandreas@gmail.com>
2023-02-10 15:01:35 +0100

    data can be downloaded and read into memory

--- CNN.py
+++ CNN.py
@@ -1,1 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+#hyper-parameters
+num_epochs = 4
+batch_size = 4
+learning_rate = 0.001
+
+#data setup
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+train_dataset = torchvision.datasets.CIFAR10(root='\data', train=True, download=True, transform=transform)
+
+test_dataset = torchvision.datasets.CIFAR10(root='\data', train=False, download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+
+classes = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+def imshow(img):
+    #img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(train_loader)
+images, labels = next(dataiter)
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
