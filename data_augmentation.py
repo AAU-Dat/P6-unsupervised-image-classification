297ea604561d59985d05a3ee6cb5b04fa84e9186
Christoffer Trebbien Jønsson <christofferctj@gmail.com>
2023-03-01 12:06:42 +0100

    Data augmentation is done. Could need some refactoring to look better. But it gets the job done

--- data_augmentation.py
+++ data_augmentation.py
@@ -16,63 +16,69 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 mnist_dataset_downloaded = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
 print("Length of mnist dataset: " + str(len(mnist_dataset_downloaded)) + " images.")
 
-# Create a loader for the mnist dataset
-mnist_dataset_loader = torch.utils.data.DataLoader(mnist_dataset_downloaded, batch_size=1, shuffle=True)
-print("Length of mnist dataset loader: " + str(len(mnist_dataset_loader)) + " batches.")
+# Create a loader for the mnist dataset without the labels
+mnist_dataset_loader = torch.utils.data.DataLoader(mnist_dataset_downloaded, batch_size=1, shuffle=False)
+print("Length of mnist dataset loader: " + str(len(mnist_dataset_loader)) + " images.")
+
 
 # Create a function that have data augmentation features
-def data_augmentation(image, label):
-    # Rotate the image
-    image = transforms.RandomRotation(360)(image)
+def data_augmentation(image, label, i):
+    # Add rotate the image
+    if i == 'data_augmentation_rotation' or i == 'data_augmentation_all_together':
+        image = transforms.RandomRotation(360)(image)
 
     # Add blur to the image
-    image = transforms.GaussianBlur(3)(image)
+    if i == 'data_augmentation_blur' or i == 'data_augmentation_all_together':
+        image = transforms.GaussianBlur(3)(image)
 
     # Add crop to the image
-    image = transforms.RandomCrop(28, padding=4)(image)
+    if i == 'data_augmentation_crop' or i == 'data_augmentation_all_together':
+        image = transforms.RandomCrop((28, 28), padding=4)(image)
 
     # Return the image and label
     return image, label
 
-# Create a folder to save the data augmentation images
-if not os.path.exists('./data_augmentation'):
-    os.makedirs('./data_augmentation')
 
-"""
-# Create a csv file to save all image
-#with open('./data_augmentation.csv', 'w') as f:
-"""
+folder_names = ['data_augmentation_rotation', 'data_augmentation_blur', 'data_augmentation_crop',
+                'data_augmentation_all_together']
 
-"""
-# Create a numpy array to save the images
-image_array = np.zeros((1, 28, 28))
-"""
+# for loop that creates a folder and use the code from 48 to 77 in each of the loops
+for i in folder_names:
+    if not os.path.exists('./' + i):
+        os.makedirs('./' + i)
 
-# Loop through the mnist dataset loader
-for i, (image, label) in enumerate(mnist_dataset_loader):
-    # Use GPU if possible
-    image = image.to(device)
-    label = label.to(device)
+    # Print the folder name
+    print("Folder: " + i)
 
-    # Save the original image
-    plt.imsave('./data_augmentation/' + str(i) + '_original.png', image[0][0], cmap='gray')
+    # Loop through the mnist dataset loader
+    for k, (image, label) in enumerate(mnist_dataset_loader):
+        # Print the progress for every 1000 images
+        if k % 1000 == 0:
+            print("Progress: " + str(k * 5) + "/" + str(len(mnist_dataset_loader) * 5))
 
-    # Create x amount of new image on the original image and save them
-    for j in range(1):
-        # Create a new image
-        new_image, new_label = data_augmentation(image, label)
+        # Save the original image to the i folder
+        plt.imsave(i + '/' + str(k) + '_original.png', image[0][0], cmap='gray')
 
-        # Save the new image
-        plt.imsave('./data_augmentation/' + str(i) + '_' + str(j) + '.png', new_image[0][0], cmap='gray')
 
-    # Print the progress
-    print("Progress: " + str(i + 1) + "/" + str(len(mnist_dataset_loader)) + " images.")
+        # Create cvs file and add the original image to it and two comma after the image name
+        with open(i + '.csv', 'a') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',')
+            csv_writer.writerow([str(k) + '_original.png', '', ''])
 
-    # Break the loop if the amount of images is 10
-    if i == 4:
-        break
+        # Create x amount of new image on the original image and save them without labels
+        for j in range(3):
 
-"""
-# Convert the numpy array to a csv file
-np.savetxt('./data_augmentation.csv', image_array, delimiter=',')
-"""
+            # Create a new image with data augmentation features
+            new_image, new_label = data_augmentation(image, label, i)
+
+            # Save the augmented image to the i folder
+            plt.imsave(i + '/' + str(k) + '_' + str(j) + '.png', new_image[0][0], cmap='gray')
+
+            # Add the new image to the csv file
+            with open(i + '.csv', 'a') as csv_file:
+                csv_writer = csv.writer(csv_file, delimiter=',')
+                csv_writer.writerow([str(k) + '_' + str(j) + '.png', '', ''])
+
+        # Break the loop if the amount of images is x
+        #if k == 1:
+        #    break
