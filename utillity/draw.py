28cdb5960d71932891fce764991f7bc0e2440844
Christoffer Trebbien Jønsson <christofferctj@gmail.com>
2023-04-04 11:57:59 +0200

    Added the rest of the features so that we can draw digits/figures and convert them to 28x28 px greyscale. It missed the functionality to save them to a folder a give them a label

--- utillity/draw.py
+++ utillity/draw.py
@@ -1,22 +1,41 @@
 import pygame
-from PIL.Image import Image
+import torchvision
 
+# Initialize the game engine
 pygame.init()
+# Set up the drawing window
 width, height = 300, 300
+# Create the screen
 screen = pygame.display.set_mode((width, height))
 
+# Background color
 WHITE = (255, 255, 255)
+# Line color
 BLACK = (0, 0, 0)
-line_width = 5
+# Line width
+line_width = 20
+# Last position
 last_pos = None
 
 
 def save_image(surface):
-    filename = 'drawing.png'
-    pygame.image.save(surface, filename)
-    print(f"Image saved as '{filename}'")
+    pygame.image.save(surface, 'temp.png')
+    save_img = torchvision.io.read_image('./temp.png', torchvision.io.ImageReadMode.GRAY)
 
+    print(f"Shape of save_img: {save_img.shape}")
 
+    # Transform the image to 28x28 pixels
+    save_img = torchvision.transforms.functional.resize(save_img, (28, 28))
+    
+    # Save the image
+    torchvision.io.write_png(save_img, './output.png')
+    
+# Clear the screen and start over
+def clear_screen():
+    # Fill background with white
+    screen.fill(WHITE)
+
+# Draw on the screen
 def draw_smth(event):
     global last_pos
     if event.type == pygame.MOUSEBUTTONDOWN:
@@ -31,6 +50,10 @@ def draw_smth(event):
     # Check for save button press
     if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
         save_image(screen)
+
+    # Check for clear button press
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
+        clear_screen()
 
 # Set up screen and caption
 pygame.display.set_caption("Drawing App")
